6c29ea19e009b284135bd674ddc63176a755f72c
Vadim Dovnar <vadim.dovnar.93@gmail.com>
2023-05-07 01:43:03 +0300

    get data from apex method in lwc

--- force-app/main/default/lwc/googleDrivePage/googleDrivePage.js
+++ force-app/main/default/lwc/googleDrivePage/googleDrivePage.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
+import getAllFiles from "@salesforce/apex/GoogleRestAPI.getAllFiles";
+export default class GoogleDrivePage extends LightningElement {
+    @track files;
 
-export default class GoogleDrivePage extends LightningElement {}
+    // @wire(getAllFiles)
+    // loadGreeting({ data, error }) {
+    //     if (data) {
+    //         this.files = JSON.parse(data);
+    //     } else if (error) {
+    //         console.error(error);
+    //     }
+    // }
+
+    connectedCallback() {
+        getAllFiles()
+            .then((result) => {
+                this.files = JSON.parse(result);
+            })
+            .catch((error) => {
+                console.error('ERROR::::::::::::::::::::::::: ' + error);
+            });
+    }
+}
