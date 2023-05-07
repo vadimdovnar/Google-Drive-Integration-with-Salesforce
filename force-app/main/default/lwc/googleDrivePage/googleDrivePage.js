eeff34b122d24186eb4425cfea8c0a5868fdcdb7
Vadim Dovnar <vadim.dovnar.93@gmail.com>
2023-05-07 04:44:42 +0300

    add links for buton and icon for the files

--- force-app/main/default/lwc/googleDrivePage/googleDrivePage.js
+++ force-app/main/default/lwc/googleDrivePage/googleDrivePage.js
@@ -17,17 +17,52 @@ export default class GoogleDrivePage extends LightningElement {
             .then((result) => {
                 this.files = JSON.parse(result);
                 this.convertDateTime(this.files);
+                this.definesIconType(this.files)
             })
             .catch((error) => {
-                console.error('ERROR::::::::::::::::::::::::: ' + error);
+                console.error("ERROR::::::::::::::::::::::::: " + error);
             });
     }
     convertDateTime(response) {
-        for(let i = 0; i < response.length; i++) {
+        for (let i = 0; i < response.length; i++) {
             let createdFileDate = new Date(response[i].createdTime);
-            response[i].createdTime = createdFileDate.toLocaleDateString('en-US');
+            response[i].createdTime = createdFileDate.toLocaleDateString("en-US");
             let modifiedFileDate = new Date(response[i].modifiedTime);
-            response[i].modifiedTime = modifiedFileDate.toLocaleDateString('en-US');
+            response[i].modifiedTime = modifiedFileDate.toLocaleDateString("en-US");
+        }
+    }
+    splitString(str) {
+        return str.match(/[a-zA-Z]+/g) || [];
+    }
+    definesIconType(response) {
+
+        for (let i = 0; i < response.length; i++) {
+            // eslint-disable-next-line default-case, no-undef
+            const words = this.splitString(response[i].mimeType);
+            // eslint-disable-next-line default-case
+            switch (words[words.length - 1]) {
+                case "sheet":
+                    response[i].iconType = "doctype:excel";
+                    break;
+                case "document":
+                    response[i].iconType = "doctype:gdoc";
+                    break;
+                case "pdf":
+                    response[i].iconType = "doctype:pdf";
+                    break;
+                case "folder":
+                    response[i].iconType = "doctype:folder";
+                    break;
+                case "presentation":
+                    response[i].iconType = "doctype:gpres";
+                    break;
+                case "shortcut":
+                    response[i].iconType = "doctype:unknown";
+                    break;
+                case "spreadsheet":
+                    response[i].iconType = "doctype:gsheet";
+                    break;
+            }
         }
     }
 }
