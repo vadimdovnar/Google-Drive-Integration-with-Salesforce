ee4a0a085061a01843dae23eb7ceab0020788fda
Vadim Dovnar <vadim.dovnar.93@gmail.com>
2023-05-07 23:04:51 +0300

    add lightning button with modal popup

--- force-app/main/default/lwc/createNewGoogleFilePopup/createNewGoogleFilePopup.js
+++ force-app/main/default/lwc/createNewGoogleFilePopup/createNewGoogleFilePopup.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
 
-export default class CreateNewGoogleFilePopup extends LightningElement {}
+export default class CreateNewGoogleFilePopup extends LightningElement {
+    @track isModalOpen = false;
+    openModal() {
+        // to open modal set isModalOpen tarck value as true
+        this.isModalOpen = true;
+    }
+    closeModal() {
+        // to close modal set isModalOpen tarck value as false
+        this.isModalOpen = false;
+    }
+    submitDetails() {
+        // to close modal set isModalOpen tarck value as false
+        //Add your code to call apex method or do some processing
+        this.isModalOpen = false;
+    }
+}
